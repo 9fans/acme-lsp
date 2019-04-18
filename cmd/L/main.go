138049160d6602a69c747e83fe27d4c47074d66c
Fazlul Shahriar <fshahriar@gmail.com>
2019-04-18 05:20:51 -0400

    cmd/L: move some functions

--- cmd/L/main.go
+++ cmd/L/main.go
@@ -10,6 +10,7 @@ import (
 
 	"9fans.net/go/acme"
 	"9fans.net/go/plan9"
+	p9client "9fans.net/go/plan9/client"
 	"9fans.net/go/plumb"
 	"github.com/fhs/acme-lsp/internal/lsp"
 	"github.com/fhs/acme-lsp/internal/lsp/client"
@@ -199,6 +200,20 @@ func plumbDefinition(c *client.Conn, pos *lsp.TextDocumentPositionParams) error 
 	return nil
 }
 
+func plumbLocation(p *p9client.Fid, loc *lsp.Location) error {
+	fn := uriToFilename(loc.URI)
+	a := fmt.Sprintf("%v:%v", fn, loc.Range.Start)
+
+	m := &plumb.Message{
+		Src:  "L",
+		Dst:  "edit",
+		Dir:  "/",
+		Type: "text",
+		Data: []byte(a),
+	}
+	return m.Send(p)
+}
+
 func formatWin(id int) error {
 	w, err := openWin(id)
 	if err != nil {
@@ -225,6 +240,25 @@ func formatWin(id int) error {
 		}
 	}()
 	return formatInEditor(s.Conn, uri, w)
+}
+
+func formatInEditor(c *client.Conn, uri lsp.DocumentURI, e editor) error {
+	edits, err := c.Format(uri)
+	if err != nil {
+		return err
+	}
+	if err := e.Edit(edits); err != nil {
+		return errors.Wrapf(err, "failed to apply edits")
+	}
+	return nil
+}
+
+func renameInEditor(c *client.Conn, pos *lsp.TextDocumentPositionParams, newname string) error {
+	we, err := c.Rename(pos, newname)
+	if err != nil {
+		return err
+	}
+	return applyAcmeEdits(we)
 }
 
 func monitor() {
