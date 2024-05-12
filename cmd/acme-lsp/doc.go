fabc4ee20cadd97d80d9d740256ff3d5696a5118
Maximilian Koeniger <maxl@x00.at>
2024-05-12 16:13:07 +0200

    doc: updated documentation

--- cmd/acme-lsp/doc.go
+++ cmd/acme-lsp/doc.go
@@ -32,21 +32,31 @@ be changed by the FormatOnPut and CodeActionsOnPut configuration options.
 		Usage: acme-lsp [flags]
 
 	  -acme.addr string
-	    	address where acme is serving 9P file system (default "/tmp/ns.fhs.:0/acme")
+	    	address where acme is serving 9P file system (default "/tmp/ns.username.:0/acme")
 	  -acme.net string
 	    	network where acme is serving 9P file system (default "unix")
 	  -debug
 	    	turn on debugging prints (deprecated: use -v)
 	  -dial value
-	    	language server address for filename match (e.g. '\.go$:localhost:4389')
+	    	map filename to language server address. The format is
+	    	'handlers:host:port'. See -server flag for format of
+	    	handlers. (e.g. '\.go$:localhost:4389')
+	  -hidediag
+	    	hide diagnostics sent by LSP server
 	  -proxy.addr string
-	    	address used for communication between acme-lsp and L (default "/tmp/ns.fhs.:0/acme-lsp.rpc")
+	    	address used for communication between acme-lsp and L (default "/tmp/ns.username.:0/acme-lsp.rpc")
 	  -proxy.net string
 	    	network used for communication between acme-lsp and L (default "unix")
 	  -rootdir string
-	    	root directory used for LSP initialization. (default "/")
+	    	root directory used for LSP initialization (default "/")
+	  -rpc.trace
+	    	print the full rpc trace in lsp inspector format
 	  -server value
-	    	language server command for filename match (e.g. '\.go$:gopls')
+	    	map filename to language server command. The format is
+	    	'handlers:cmd' where cmd is the LSP server command and handlers is
+	    	a comma separated list of 'regexp[@lang]'. The regexp matches the
+	    	filename and lang is a language identifier. (e.g. '\.go$:gopls' or
+	    	'go.mod$@go.mod,go.sum$@go.sum,\.go$@go:gopls')
 	  -showconfig
 	    	show configuration values and exit
 	  -v	Verbose output
